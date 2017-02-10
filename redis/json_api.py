9b769eeaaeef39fe05611aad9d435558755ffa59
rdarbha <rama.h.darbha@gmail.com>
2017-02-09 20:55:21 -0500

    Testing system call to gather netq output

--- redis/json_api.py
+++ redis/json_api.py
@@ -1,11 +1,14 @@
 import redis
-from subprocess import call
+import subprocess
+import json
 
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
 #print redis_db.keys()
 
-commands = ["ls -l","ls"]
+#commands = ["ls -l","ls"]
+commands = ["net"]
 
 for command in commands:
   #call(["ls", "-l"])
-  call(command.split())
+  result = subprocess.check_output(batcmd, shell=True)
+  print result
