be3f97c4039b2c753c37b8ee48073db416562aa3
slaffer-au <slaffer.au@gmail.com>
2017-05-08 06:32:06 +0000

    With the file I forgot

--- roles/monitoring_agent/files/telegraf_checks/logs_data.py
+++ roles/monitoring_agent/files/telegraf_checks/logs_data.py
@@ -17,13 +17,16 @@ def parse_logs():
 
     for line in Pygtail("/var/log/syslog"):
         # print line
-        if "sent to neighbor" in line:
-            reason = line.split('(')[1].split(')')[0]
-        if "Down BGP Notification" in line:
+        #if "sent to neighbor" in line:
+            #reason = line.split('(')[1].split(')')[0]
+        if "%ADJCHANGE: neighbor" in line:
             # print "***found*** " + '"'+str(line.split(' ')[5])+'"'
-            peer = line.split(' ')[5]
+            msg = line.split()
+            peer = msg[5]
+            status = msg[9]
+            reason = msg[-2]+" "+msg[-1]
         if len(reason) > 0 and len(peer) > 0:
-            data.add_row({"msg":"log"},{"reason":'"'+reason+" on "+gethostname()+" from "+peer+'"',"peer":'"'+peer+'"'})
+            data.add_row({"msg":"log"},{"reason":'"BGP Neighbor '+status+": "+reason+" on "+gethostname()+" from "+peer+'"',"peer":'"'+peer+'"'})
             reason = ""
             peer = ""
 
